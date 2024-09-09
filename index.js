ad9480b110a7b6c4b19133f1cb4626c92a202c20
indiphilegogela <156888615+indiphilegogela@users.noreply.github.com>
2024-09-09 15:58:56 +0200

    Update index.js
    I added each characters favourite genre

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "R&B",
+    "Rocket": "Hip-Hop",
+    "Groot":"House",
     // Add preferences for Drax, Rocket, and Groot
 };
 
